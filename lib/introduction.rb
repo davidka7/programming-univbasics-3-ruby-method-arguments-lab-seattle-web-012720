98f10387d28157f45cee34d9e054a7166ce5c8b3
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-09 07:42:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,7 @@ def introduction(david)
 puts "Hi, my name is #{name}."
 end
 def introduction_with_language(david, ruby)
-puts "Hi, my name is #{name} and I am learning to program in #{language
+puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
 
